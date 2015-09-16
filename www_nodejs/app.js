0c6f6cf42a5451e931b0cb164e362064a4b6d8cc
碌木 <srleo@qq.com>
2015-09-16 21:54:24 +0800

    1

--- www_nodejs/app.js
+++ www_nodejs/app.js
@@ -47,7 +47,7 @@ var server = http.createServer(function (request, response) {
 
     if (isFail) {//fail
         response.writeHead(404, {'Content-Type': 'text/plain'});
-        response.end();
+        response.end('404:'+host+indexFile);
     }
 });
 
