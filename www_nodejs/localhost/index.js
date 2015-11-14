f4687b3c2cda3879e3f91b251f3c90c248c3c66c
yeoman <srleo@qq.com>
2015-11-15 01:40:26 +0800

    Update index.js

--- www_nodejs/localhost/index.js
+++ www_nodejs/localhost/index.js
@@ -1,13 +1,25 @@
 var express = require('express');
 
+/*
+var mongoose = require('mongoose');
+var Video = require('./models/Video.js');
+mongoose.connect('mongodb://mongodb/dbname');
+*/
+
 var app = express();
+
+app.onClearCache = function(){
+    /*
+     * If index.js use mongoose,then must delete mongoose model and close mongoose conect
+     * Because index.js will recreate Video model and reconnect MongoDB
+     * for example:
+        delete mongoose.connection.models['Video'];//remove mongoose model 'Video'
+        mongoose.disconnect();//close mongoose all connect;
+    */
+};
 
 app.get('/', function (req, res) {
     res.end('Hello, World!');
-});
-
-app.get('/node', function (req, res) {
-    res.end('/node');
 });
 
 module.exports = app;
